ba32318d79d7ab2e26f9c2aa78f30662e8f59883
dannyid <danielshekhtman@gmail.com>
2013-08-14 19:12:23 -0400

    some stuff

--- js/chess.js
+++ js/chess.js
@@ -1,9 +1,24 @@
-$( "img" ).click(function(evt) {
-    var $parent = $(evt.target).parent();
-    console.log($parent.css('background-color'));
-    if ($parent.css("background-color") !== "rgb(255, 0, 0)") {
-        $parent.css({"background-color": "red"});
+
+$( "td" ).click(function(evt) {
+    var cell = $(evt.currentTarget);
+    console.log(evt);
+    console.log($(evt));
+    console.log(evt.target);
+    console.log(cell);
+    if (cell.css("background-color") !== "rgb(255, 0, 0)") {
+        cell.css({"background-color": "red"});
     } else {
-        $parent.css({"background-color": $parent.hasClass("black") ? "grey" : "white" });
+        cell.css({"background-color": cell.hasClass("black") ? "grey" : "white" });
     };
 });
+
+
+//$( "img" ).click(function(evt) {
+//    var $parent = $(evt.target).parent();
+//    console.log($parent.css('background-color'));
+//    if ($parent.css("background-color") !== "rgb(255, 0, 0)") {
+//        $parent.css({"background-color": "red"});
+//    } else {
+//        $parent.css({"background-color": $parent.hasClass("black") ? "grey" : "white" });
+//    };
+//});
